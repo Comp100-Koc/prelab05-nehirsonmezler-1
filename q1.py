8b7d5d3dab1e6d27dffa26ea20aebd07500d4a0d
nehir <nsonmezler25@ku.edu.tr>
2026-04-03 01:17:23 +0300

    Add longest_palindromic_substring function

--- q1.py
+++ q1.py
@@ -2,4 +2,16 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    longest = ""
+    n = len(s)
+    
+    for i in range(n):
+        for j in range(i + 2, n + 1):  
+            sub = s[i:j]
+            
+            if sub == sub[::-1]:  
+                if len(sub) > len(longest):
+                    longest = sub
+    
+    return longest
+    
