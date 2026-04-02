5b52e5291e6beac559f31c778f2df1b4e54b6b75
nehir <nsonmezler25@ku.edu.tr>
2026-04-03 01:18:38 +0300

    Add function to remove adjacent duplicates from string

--- q2.py
+++ q2.py
@@ -2,4 +2,21 @@ def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    changed = True
+    
+    while changed:
+        changed = False
+        i = 0
+        new_s = ""
+        
+        while i < len(s):
+            if i < len(s) - 1 and s[i] == s[i+1]:
+                changed = True
+                i += 2  
+            else:
+                new_s += s[i]
+                i += 1
+        
+        s = new_s
+    
+    return s
