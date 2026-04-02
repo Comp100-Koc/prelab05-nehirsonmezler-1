955fc5fd40580b5ec44cad7dc7746b774dca4e96
nehir <nsonmezler25@ku.edu.tr>
2026-04-03 01:22:44 +0300

    Add binary addition functionality to add_binary

--- q3b.py
+++ q3b.py
@@ -2,4 +2,38 @@ def add_binary(a, b):
     '''
     Given two strings perform binary addition and return the result as a string
     '''
-    pass
+    a = a[2:]
+    b = b[2:]
+    
+    i = len(a) - 1
+    j = len(b) - 1
+    carry = 0
+    result = ""
+    
+    while i >= 0 or j >= 0 or carry:
+        total = carry
+        
+        if i >= 0:
+            if a[i] == '1':
+                total += 1
+            i -= 1
+        
+        if j >= 0:
+            if b[j] == '1':
+                total += 1
+            j -= 1
+        
+        if total == 0:
+            result = "0" + result
+            carry = 0
+        elif total == 1:
+            result = "1" + result
+            carry = 0
+        elif total == 2:
+            result = "0" + result
+            carry = 1
+        else:
+            result = "1" + result
+            carry = 1
+    
+    return "0b" + result
