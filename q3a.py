cb29f2b02e8f3a5b967ad1f943cf87b1b8eedaf9
nehir <nsonmezler25@ku.edu.tr>
2026-04-03 01:23:17 +0300

    Simplify binary addition using built-in function

--- q3a.py
+++ q3a.py
@@ -1,36 +1,3 @@
 def add_binary(a, b):
-    a = a[2:]
-    b = b[2:]
-    
-    i = len(a) - 1
-    j = len(b) - 1
-    carry = 0
-    result = ""
-    
-    while i >= 0 or j >= 0 or carry:
-        total = carry
+   return bin(int(a, 2) + int(b, 2))
         
-        if i >= 0:
-            if a[i] == '1':
-                total += 1
-            i -= 1
-        
-        if j >= 0:
-            if b[j] == '1':
-                total += 1
-            j -= 1
-        
-        if total == 0:
-            result = "0" + result
-            carry = 0
-        elif total == 1:
-            result = "1" + result
-            carry = 0
-        elif total == 2:
-            result = "0" + result
-            carry = 1
-        else:  
-            result = "1" + result
-            carry = 1
-    
-    return "0b" + result
